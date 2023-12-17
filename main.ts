7e716f057738a0da7087aab43be79e3851db24a8
Ben Scofield <git@turrean.com>
2023-12-17 13:56:01 -0500

    Initial commit

--- main.ts
+++ main.ts
@@ -2,80 +2,102 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface DoiImporterSettings {
+	referenceNotePath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DoiImporterSettings = {
+	referenceNotePath: '/references'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DoiImporter extends Plugin {
+	settings: DoiImporterSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'import-doi',
+			name: 'Import reference from DOI',
+			hotkeys: [
+				{
+					modifiers: ['Meta', 'Shift'],
+					key: 'i',
+				},
+			],
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				const doi = editor.getSelection()
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				// validate that it's a DOI
+				console.log(`Fetching metadata for ${doi}`)
+
+				// get the metadata for the DOI
+				const url = `https://api.crossref.org/works/${doi}`
+				const data = fetch(url)
+					.then(response => response.json())
+					.then(data => {
+						const metadata = data.message;
+
+						const notePath = `${this.settings.referenceNotePath}/${metadata.title}.md`
+						const firstAuthor = metadata.author[0].family.toLowerCase()
+
+						const year = metadata.created['date-parts'][0][0]
+						const alias = `${firstAuthor}${year}`
+
+						const authors = metadata.author.map((author: any) => {
+							return `${author.given} ${author.family}`
+						}).join(', ')
+
+						const contents = {
+							title: metadata.title[0],
+							authors,
+							year,
+							journal: metadata['container-title'][0],
+							volume: metadata.volume,
+							issue: metadata.issue,
+							page: metadata.page,
+							doi: metadata.DOI,
+							url: metadata.URL
+						}
+
+						// create the note
+							const note = `# ${contents.title}
+
+- Author: ${contents.authors}
+- Year: ${contents.year}
+- Journal: ${contents.journal}
+- Pages: ${contents.page}
+- DOI: ${contents.doi}
+- URL: ${contents.url}
+
+## Summary
+
+TK
+`;
+
+						// if note doesn't exist, create it
+						if (!this.app.vault.getAbstractFileByPath(notePath)) {
+							this.app.vault.create(notePath, note).then((file) => {
+								this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+									frontmatter.aliases = [alias, contents.doi]
+								})
+							})
+
+							// replace the selection with the note link
+							const link = `[[${contents.title}|${contents.doi}]]`
+							editor.replaceSelection(link)
+						} else {
+							new Notice('Note already exists')
+						}
+					}
+				)
 			}
 		});
+
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new DoiImporterSettingsTab(this.app, this));
 	}
 
 	onunload() {
@@ -107,10 +129,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class DoiImporterSettingsTab extends PluginSettingTab {
+	plugin: DoiImporter;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DoiImporter) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +143,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Reference Note Path')
+			.setDesc('Where would you like to save reference notes?')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter the path')
+				.setValue(this.plugin.settings.referenceNotePath)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.referenceNotePath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
